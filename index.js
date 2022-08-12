cb22ba75215848a2dce1c2d7aeff69269024cd27
tenmark86e <tenmark86@gmail.com>
2022-08-12 14:25:20 +0300

    Done Using the Array Filter Method assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+//✔ returns all drivers that match the passed in name
+const findMatching = (p,q) => p.filter(p => p.toLowerCase() === q.toLowerCase())
+
+//✔ returns a driver if beginning provided letters match
+const fuzzyMatch = (p,q) => p.filter(p => p.charAt(0) === q.charAt(0))
+
+//✔ accesses the data structure to check if name match
+const matchName = (p,q) => p.filter(p => p.name === q)
